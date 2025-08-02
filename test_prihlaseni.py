154ae06f030c5f3edfcc5d203d407dad1a63b790
Vojta <cermakvoj@gmail.com>
2025-08-02 21:17:05 +0200

    Update GitHub Actions to run all Playwright tests

--- test_prihlaseni.py
+++ test_prihlaseni.py
@@ -2,10 +2,8 @@ from playwright.sync_api import sync_playwright
 
 def test_homepage_loads():
     with sync_playwright() as p:
-        browser = p.webkit.launch(headless=False, slow_mo=1000)
+        browser = p.chromium.launch(headless=False, slow_mo=1000)
         page = browser.new_page()
-        print("Načítám stránku...")
         page.goto("http://37.27.17.198:8084/cs/")
-        print("Stránka načtena:", page.title())
+        assert "Přihlášení" in page.title()
         browser.close()
-        browser.stop_tracing
